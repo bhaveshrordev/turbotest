eb8fcc8ceaccfff4f3e88de8165c4c77fae546ef
bhaveshrordev <salujabhavesh24@gmail.com>
2024-03-02 14:55:01 +0530

    added search movies by title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,22 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def search
+    if params[:title_search].present?
+      @movies = Movie.filter_by_title(params[:title_search])
+    else
+      @movies = []
+    end
+    
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("search_results", 
+                                                  partial: "movies/search_results", 
+                                                  locals: { movies: @movies }) 
+      end
+    end
+  end
+
   # GET /movies/1 or /movies/1.json
   def show
   end
