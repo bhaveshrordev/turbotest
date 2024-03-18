94e7c9de0158b1b96c741697f556a4d3f3a7e0c0
bhaveshrordev <salujabhavesh24@gmail.com>
2024-03-18 20:15:18 +0530

    added seed for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# 150.times do
-# 	Movie.create(title: Faker::Movie.unique.title)
-# end
+150.times do
+  Movie.create(title: Faker::Movie.unique.title)
+end
 
 150.times do
   Comment.create(body: Faker::Quote.famous_last_words)
